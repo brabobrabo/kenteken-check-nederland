a1a2c8ac3b5456c9ca9548f1b2f55aba74f234e1
brabobrabo <40462686+brabobrabo@users.noreply.github.com>
2025-06-02 18:35:57 +0000

    needed to edit the vite.config.ts file : /kenteken-check-nederland/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // 👈 required for GitHub Pages
+  base: "/kenteken-check-nederland/", // 👈 required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
